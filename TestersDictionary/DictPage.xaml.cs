7ba005a31b153a9683920fb3f53150a6fdb20f0f
Никита Рубцов <kadirmc@mail.ru>
2024-03-28 18:13:22 +0300

    Добавьте файлы проекта.

---
+++ TestersDictionary/DictPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestersDictionary
+{
+    /// <summary>
+    /// Логика взаимодействия для DictPage.xaml
+    /// </summary>
+    public partial class DictPage : Page
+    {
+        public DictPage()
+        {
+            InitializeComponent();
+            DataGridWords.ItemsSource = Entities.GetContext().Words.ToArray();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegPage());
+
+            
+        }
+
+        private void DataGridWords_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // pass
+        }
+
+        private void DelButton_Click(object sender, RoutedEventArgs e)
+        {
+            var wordsForRemoving = DataGridWords.SelectedItems.Cast<Words>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить записи в количестве {wordsForRemoving.Count()} элемента(-ов)?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    Entities.GetContext().Words.RemoveRange(wordsForRemoving);
+                    Entities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные успешно удалены!");
+
+                    DataGridWords.ItemsSource = Entities.GetContext().Words.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+
+        }
+    
+    }
+}
---
+++ TestersDictionary/RegPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestersDictionary
+{
+    /// <summary>
+    /// Логика взаимодействия для RegPage.xaml
+    /// </summary>
+    public partial class RegPage : Page
+    {
+        private Words _currentWords = new Words();
+        public RegPage()
+        {
+            InitializeComponent();
+            DataContext = _currentWords;
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new DictPage());
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder(); 
+            if (string.IsNullOrEmpty(_currentWords.Определение)) errors.AppendLine("Укажите логин!");
+            if (string.IsNullOrEmpty(_currentWords.Источники)) errors.AppendLine("Укажите источник!");
+            if (string.IsNullOrEmpty(_currentWords.Понятие)) errors.AppendLine("Укажите понятие!");
+
+
+
+            if (errors.Length > 0) { MessageBox.Show(errors.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning); }
+            else { 
+                Entities.GetContext().Words.Add(_currentWords); 
+                try
+                {
+                    Entities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные успешно сохранены!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+                    NavigationService.Navigate(new DictPage());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+        }
+    }
+}
